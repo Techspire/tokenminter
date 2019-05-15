6e84ab49613cfd6596b02f52e8f4504224fa8c53
Erik Marks <rekmarks@protonmail.com>
2019-05-15 11:29:28 -0400

    Updates

---
+++ precache-manifest.2e889efd55a6a2696eff47f567cd52a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1b79be88eaf33b8e03ef1a24419c4d",
+    "url": "/tokenminter/index.html"
+  },
+  {
+    "revision": "298c78842073d997341a",
+    "url": "/tokenminter/static/css/main.6a1ffe12.chunk.css"
+  },
+  {
+    "revision": "c821c147970dea8cae4d",
+    "url": "/tokenminter/static/js/2.b1979522.chunk.js"
+  },
+  {
+    "revision": "298c78842073d997341a",
+    "url": "/tokenminter/static/js/main.af7477c8.chunk.js"
+  },
+  {
+    "revision": "9e9f148136a9e0f94e52",
+    "url": "/tokenminter/static/js/runtime~main.208d2fd6.js"
+  }
+]);
